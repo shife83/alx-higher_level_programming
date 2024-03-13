808ba2911761864c3d2938d3684c507c801c7517
shife83 <shiferaw83@gmail.com>
2024-03-12 23:50:37 -0700

    1-last_digit.py some syntax error fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)  # Generate a random number
-# Extract the last digit using the modulo operator
+number = random.randint(-10000, 10000)
 last_digit = number % 10
-# Check the last digit and print the appropriate message
 print(f"Last digit of {number} is {last_digit}", end="")
 if last_digit > 5:
    print(" and is greater than 5")
